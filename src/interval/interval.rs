f73d039ab940911e91c0c8bc13691b873f7dad60
Bradford Toney <bradford.toney@gmail.com>
2022-08-12 01:14:47 -0400

    feat: cleanup + tests

--- src/interval/interval.rs
+++ src/interval/interval.rs
@@ -89,8 +89,36 @@ impl Iterator for Interval {
     type Item = Interval;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let interval = self;
+        let interval = self.clone();
+        self.start = interval.start + self.duration;
 
-        Some(interval.clone())
+        Some(interval)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interval() {
+        let start = NaiveDate::from_ymd(2022, 1, 1);
+        let duration = RelativeDuration::months(1);
+
+        let mut interval = Interval::from_start(start, duration);
+
+        assert_eq!(interval.start_date(), Some(start));
+        assert_eq!(interval.end_date(), Some(NaiveDate::from_ymd(2022, 2, 1)));
+
+        // Intervals are inclusive so return the current time span first
+        let next = interval.next().unwrap();
+
+        assert_eq!(next.start_date(), Some(NaiveDate::from_ymd(2022, 1, 1)));
+        assert_eq!(next.end_date(), Some(NaiveDate::from_ymd(2022, 2, 1)));
+
+        let next = interval.next().unwrap();
+
+        assert_eq!(next.start_date(), Some(NaiveDate::from_ymd(2022, 2, 1)));
+        assert_eq!(next.end_date(), Some(NaiveDate::from_ymd(2022, 3, 1)));
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+//! # calends
+//!
+//! calends is a library for durations, intervals and other calendar related operations. It is
+//! designed to work with chrono.
+//!
+//! # Durations of time
+//!
+//! A duration is a unit of time that has some ability to be applied to a date to produce another
+//! date.
+//!
+//! ```
+//! use calends::RelativeDuration;
+//! use chrono::NaiveDate;
+//!
+//! // This will allow you to add one month and then go back two days from the added month
+//! let rd = RelativeDuration::months(1).with_days(-2);
+//!
+//! // It also compatible with NaiveDate
+//! assert_eq!(
+//!     NaiveDate::from_ymd(2022, 1, 1) + rd,
+//!     NaiveDate::from_ymd(2022, 1, 30)
+//! );
+//! ```
+//!
+//! # Recurrence & Rules
+//!
+//! Recurrence allows you to specify a ruleset for how events (dates) repeat in time.
+//!
+//! ```
+//! use calends::{Recurrence, Rule};
+//! use chrono::NaiveDate;
+//!
+//! let date = NaiveDate::from_ymd(2022, 1, 1);
+//! let end = NaiveDate::from_ymd(2022, 3, 1);
+//!
+//! let mut recur = Recurrence::with_start(Rule::monthly(), date).until(end);
+//! assert_eq!(recur.next(), Some(NaiveDate::from_ymd(2022, 1, 1)));
+//! assert_eq!(recur.next(), Some(NaiveDate::from_ymd(2022, 2, 1)));
+//! assert_eq!(recur.next(), None);
+//! ```
+//!
+
 pub mod duration;
 pub mod event;
 pub mod interval;
@@ -5,6 +47,6 @@ pub mod recurrence;
 pub mod util;
 
 pub use crate::duration::RelativeDuration;
-pub use crate::event::*;
-pub use crate::recurrence::*;
+pub use crate::recurrence::Recurrence;
+pub use crate::recurrence::Rule;
 pub use crate::util::*;
