e9b6ee31ed6e4a5f89da86824d4282bc60e4e9d1
Bradford Toney <bradford.toney@gmail.com>
2022-09-06 13:33:41 -0400

    fix: add the rest of the parser

--- src/duration/parse.rs
+++ src/duration/parse.rs
@@ -1,11 +1,10 @@
 use nom::{
-    bytes::complete::{tag, take_while, take_while_m_n},
+    bytes::complete::{tag, take_while},
     character::{
         complete::{char, one_of},
         is_digit,
     },
     combinator::opt,
-    complete::take,
     error::Error,
     multi::count,
     sequence::{preceded, tuple},
@@ -42,22 +41,25 @@ fn take_signed_digits(i: &[u8]) -> IResult<&[u8], i32> {
 }
 
 fn parse_duration_chunk(input: &[u8]) -> IResult<&[u8], Unit> {
-    match tuple((take_signed_digits, one_of("YMWD")))(input)? {
-        (i, (amt, u)) => match u {
-            'Y' => Ok((i, Unit::Years(amt))),
-            'M' => Ok((i, Unit::Months(amt))),
-            'W' => Ok((i, Unit::Weeks(amt))),
-            'D' => Ok((i, Unit::Days(amt))),
-            _ => Err(Err::Error(Error::new(i, nom::error::ErrorKind::Fail))),
-        },
+    let (i, (amt, u)) = tuple((take_signed_digits, one_of("YMWD")))(input)?;
+    match u {
+        'Y' => Ok((i, Unit::Years(amt))),
+        'M' => Ok((i, Unit::Months(amt))),
+        'W' => Ok((i, Unit::Weeks(amt))),
+        'D' => Ok((i, Unit::Days(amt))),
+        _ => Err(Err::Error(Error::new(i, nom::error::ErrorKind::Fail))),
     }
 }
 
+/// Parse a duration
+///
 pub fn parse_relative_duration(input: &str) -> IResult<&[u8], RelativeDuration> {
-    let (leftover, units) = preceded(tag(b"P"), count(parse_duration_chunk, 4))(input.as_bytes())?;
+    let (leftover, units) =
+        preceded(tag("P"), count(opt(parse_duration_chunk), 4))(input.as_bytes())?;
 
     let rd = units
         .iter()
+        .flatten()
         .fold(RelativeDuration::default(), |start, unit| match unit {
             Unit::Years(y) => start.with_months(y * 12),
             Unit::Months(m) => start.with_months(*m),
@@ -86,7 +88,7 @@ mod tests {
 
     #[test]
     fn test_parse_duration_chunk_weeks() {
-        let (_input, duration) = parse_duration_chunk("-1M".as_bytes()).unwrap();
+        let (_input, duration) = parse_duration_chunk("-1W".as_bytes()).unwrap();
         assert_eq!(duration, Unit::Weeks(-1))
     }
 
@@ -94,5 +96,14 @@ mod tests {
     fn test_parse_duration_chunk_days() {
         let (_input, duration) = parse_duration_chunk("180D".as_bytes()).unwrap();
         assert_eq!(duration, Unit::Days(180))
+    }
+
+    #[test]
+    fn test_parse_duration() {
+        let (_input, duration) = parse_relative_duration("P3W2D").unwrap();
+        assert_eq!(
+            duration,
+            RelativeDuration::default().with_weeks(3).with_days(2)
+        )
     }
 }
